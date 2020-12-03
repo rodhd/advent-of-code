4c5322e8d0f59f586ef8374182d4cfc8eb5e6539
Rodrigo H <46557955+rodhd@users.noreply.github.com>
2020-12-03 10:06:18 +0100

    day 3

---
+++ Day3-TobogganTrajectory/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace Day3_TobogganTrajectory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "/Users/user/Projects/personal/advent-of-code/Day3-TobogganTrajectory/Input.txt";
+            string[] lines = System.IO.File.ReadAllLines(path);
+
+            var answer1 = TrajectoryFinder.Slide(lines, 0, 3, 1);
+            Console.WriteLine($"First answer: {answer1} trees");
+
+            Tuple<int, int>[] patterns = new[]
+            {
+                new Tuple<int, int>(1, 1),
+                new Tuple<int, int>(3, 1),
+                new Tuple<int, int>(5, 1),
+                new Tuple<int, int>(7, 1),
+                new Tuple<int, int>(1, 2)
+            };
+
+            var answer2 = TrajectoryFinder.SlideMany(lines, patterns);
+            Console.WriteLine($"Second answer: {answer2}");
+
+        }
+    }
+}
---
+++ Day3-TobogganTrajectory/TrajectoryFinder.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day3_TobogganTrajectory
+{
+    public static class TrajectoryFinder
+    {
+        public static int Slide(string[] input, int x0, int horizontalIncrement, int verticalIncrement)
+        {
+            var count = 0;
+            var x = x0;
+            for (int y = 0; y < input.Length; y += verticalIncrement)
+            {
+                if (input[y][x].Equals(Char.Parse("#")))
+                {
+                    count += 1;
+                }
+
+                if (x + horizontalIncrement < input[y].Length)
+                {
+                    x += horizontalIncrement;
+                }
+                else
+                {
+                    x += horizontalIncrement - (input[y].Length);
+                }
+            }
+
+            return count;
+        }
+
+        public static Int64 SlideMany(string[] input, Tuple<int, int>[] patterns)
+        {
+            List<int> result = new List<int>();
+
+            foreach (var p in patterns)
+            {
+                result.Add(Slide(input, 0 , p.Item1, p.Item2));
+            }
+
+            Int64 mult = result.Aggregate(1, (total, next) => total * next);
+            return mult;
+        }
+    }
+}
